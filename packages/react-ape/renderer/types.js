bf0d799a5a3c09cce9be97ce771e858401a0bb66
Fabio M. Costa <fabiomcosta@gmail.com>
2018-07-09 22:28:10 -0700

    Adding some flow definitions just for starters

---
+++ packages/react-ape/renderer/types.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2018-present, Raphael Amorim.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ *
+ */
+
+export type CanvasComponentContext = {
+  _renderQueueForUpdate: Array<mixed>,
+  type: 'canvas',
+  ctx: CanvasRenderingContext2D
+};
