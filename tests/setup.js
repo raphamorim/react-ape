6ba1adecd80161b474146422b3990ace6c5654ea
John-Paul Harold <johnpaul.harold@gmail.com>
2021-01-29 21:36:03 +0000

    prettier pass

--- tests/setup.js
+++ tests/setup.js
@@ -7,13 +7,10 @@ const { window } = jsdom;
 function copyProps(src, target) {
   const props = Object.getOwnPropertyNames(src)
     .filter(prop => typeof target[prop] === 'undefined')
-    .reduce(
-      (result, prop) => ({
-        ...result,
-        [prop]: Object.getOwnPropertyDescriptor(src, prop)
-      }),
-      {}
-    );
+    .reduce((result, prop) => ({
+      ...result,
+      [prop]: Object.getOwnPropertyDescriptor(src, prop)
+    }),{});
   Object.defineProperties(target, props);
 }
 
