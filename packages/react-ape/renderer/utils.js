53f857082144764ac799a2e0a695a459bcbd5e09
Raphael Amorim <rapha850@gmail.com>
2022-07-12 13:43:31 +0200

    isWebAssemblySupported handler

--- packages/react-ape/renderer/utils.js
+++ packages/react-ape/renderer/utils.js
@@ -6,3 +6,16 @@
 export function unsafeCreateUniqueId(): string {
   return (Math.random() * 10e18 + Date.now()).toString(36);
 }
+
+export const isWebAssemblySupported = (() => {
+    try {
+        if (typeof WebAssembly === "object"
+            && typeof WebAssembly.instantiate === "function") {
+            const module = new WebAssembly.Module(Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
+            if (module instanceof WebAssembly.Module)
+                return new WebAssembly.Instance(module) instanceof WebAssembly.Instance;
+        }
+    } catch (e) {
+    }
+    return false;
+})();
